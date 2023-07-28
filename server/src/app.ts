57454789f44a2ab89a245ff67025e9c2f6c21e08
hamelln <vusdus91@gmail.com>
2023-07-28 13:52:25 +0900

    feat: cors 처리 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -26,6 +26,7 @@ app.use(
 app.get("/", (req, res) => {
   res.json({ a: "12" });
 });
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
