7d707a08a31f8381d966a62069be0c7972e223b7
hamelln <vusdus91@gmail.com>
2023-08-02 15:20:32 +0900

    refactor: 컴포넌트 분리 - 좋아요 버튼, Contact 섹션은 상호 작용 컴포넌트 -> CSR 컴포넌트로서 분리

--- client/app/search/[id]/page.tsx
+++ client/app/search/[id]/page.tsx
@@ -1,84 +1,50 @@
 import React from "react";
-import IBrewery from "@/types/Brewery";
-import S from "./brewery.module.scss";
+import Brewery from "@/types/Brewery";
+import S from "./Brewery.module.scss";
 import BreweriesApi from "@/services/BreweriesApi";
 import Notfound from "./not-found";
 import Carousel from "@/app/search/carousel/Carousel";
+import StarButton from "./star/StarButton";
+import Contact from "./Contact";
 
 interface Props {
   params: { id: string };
 }
 
-export default async function Brewery({ params }: Props) {
+export default async function BreweryDetails({ params }: Props) {
   const breweriesApi = new BreweriesApi();
   const id = params.id;
-  const breweryInfo: IBrewery | null = await breweriesApi.fetchBreweryById(id);
+  const breweryInfo: Brewery | null = await breweriesApi.fetchBreweryById(id);
   if (!breweryInfo) return <Notfound />;
-  const { name, beer_description } = breweryInfo.signature_beer;
-  const phoneNumber = breweryInfo.phone.replaceAll("-", "");
+  const {
+    breweryName,
+    breweryDescription,
+    stateProvince,
+    city,
+    address1,
+    phone,
+    websiteUrl,
+  } = breweryInfo;
+  const { beerName, beerDescription } = breweryInfo.signatureBeer;
+  const images = ["/brewery-image.png", "/brewery-image.png"];
 
   return (
     <article className={S.main}>
-      <Carousel />
+      <Carousel images={images} />
       <header className={S.title_header}>
-        <h2 className={S.title}>{breweryInfo.name}</h2>
-        <button className={S.like_box}>
-          <img
-            className={S.like_icon}
-            src="/images/icons/love.png"
-            alt="Like icon"
-          />
-        </button>
+        <h2 className={S.title}>{breweryName}</h2>
+        <StarButton />
       </header>
-      <section className={S.info_section}>
-        <address className={S.address_box}>
-          <img
-            className={S.address_image}
-            src="/images/icons/location.png"
-            alt="Address icon"
-          />
-          <span>
-            {breweryInfo.state_province} {breweryInfo.city}
-            {breweryInfo.address_1}
-          </span>
-          <button className={S.copy_address_button}>
-            <img src="/images/icons/copy.png"></img>
-          </button>
-        </address>
-        <div className={S.office_hours_box}>
-          <img
-            className={S.office_hours_image}
-            src="/images/icons/clock.png"
-            alt="Clock icon"
-          />
-          <time className={S.todays_office_hours}>12:00 - 23:00</time>
-          <time className={S.todays_break_time}>16:00 - 17:00</time>
-          <button className={S.other_office_hours}>+</button>
-        </div>
-        <div className={S.phone_box}>
-          <img
-            className={S.phone_image}
-            src="/images/icons/phone.png"
-            alt="Phone icon"
-          />
-          <a href={`tel:${phoneNumber}`} className={S.phone_number}>
-            {breweryInfo.phone}
-          </a>
-        </div>
-        <div className={S.site_box}>
-          <img
-            className={S.site_image}
-            src="/images/icons/link.png"
-            alt="Website icon"
-          />
-          <a href={breweryInfo.website_url} className={S.site_url}>
-            {breweryInfo.website_url}
-          </a>
-        </div>
-      </section>
+      <Contact
+        stateProvince={stateProvince}
+        city={city}
+        address1={address1}
+        phone={phone}
+        websiteUrl={websiteUrl}
+      />
       <div className={S.cutline}></div>
       <section className={S.description_section}>
-        <p>{breweryInfo.brewery_description}</p>
+        <p>{breweryDescription}</p>
       </section>
       <div className={S.cutline}></div>
       <section className={S.recommend_section}>
@@ -86,13 +52,13 @@ export default async function Brewery({ params }: Props) {
           <div className={S.beer_title_box}>
             <img
               className={S.beer_icon}
-              src="/images/icons/beer.png"
+              src="/images/icons/beer.svg"
               alt="Beer icon"
             />
             <span className={S.beer_recommendation}>추천 맥주</span>
           </div>
-          <h3 className={S.beer_name}>{name}</h3>
-          <p>{beer_description}</p>
+          <h3 className={S.beer_name}>{beerName}</h3>
+          <p>{beerDescription}</p>
         </div>
       </section>
     </article>
