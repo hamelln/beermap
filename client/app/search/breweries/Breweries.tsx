cc190925f8fac3b21ae214989f52263cbca903ab
hamelln <vusdus91@gmail.com>
2023-08-02 16:09:01 +0900

    refactor: Breweries 컴포넌트 분리, handleClick 기능 분리 - Breweries -> Breweries, BreweryCard로 분리 - handleClick에서 잡다하게 수행하고 있는 상황 - 이전 정보를 저장하기만 하는 saveSearchInfo 함수를 BreweryCard에 전달 - BreweryCard의 handleClick은 saveSearchInfo 사용, 상세 페이지로 라우팅

--- client/app/search/breweries/Breweries.tsx
+++ client/app/search/breweries/Breweries.tsx
@@ -1,5 +1,4 @@
-import React, { MouseEvent } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
 import S from "./Breweries.module.scss";
 import Brewery from "@/types/Brewery";
 import {
@@ -7,6 +6,7 @@ import {
   saveKeyword,
   saveScrollPosition,
 } from "@/utils/search-result-cacher";
+import BreweryCard from "./brewery-card/BreweryCard";
 
 interface Props {
   inputText: string;
@@ -14,58 +14,20 @@ interface Props {
 }
 
 const Breweries = ({ inputText, breweries }: Props) => {
-  const router = useRouter();
-
-  const handleClick = (e: MouseEvent<HTMLLIElement>, breweryId: string) => {
-    e.preventDefault();
+  const saveSearchInfo = () => {
     saveKeyword(inputText);
     saveBreweries(breweries);
     saveScrollPosition(window.scrollY);
-    router.push(`/search/${breweryId}`);
   };
 
   return (
     <section className={S.section}>
       <ul className={S.brewery_list} data-testid="searchResult">
-        {breweries.map(
-          ({
-            id,
-            breweryName,
-            breweryDescription,
-            signatureBeer,
-            stateProvince,
-            city,
-            address1,
-          }: Brewery) => {
-            const fullAddress = `${stateProvince} ${city} ${address1}`;
-            return (
-              <li
-                className={S.brewery_item}
-                key={id}
-                onClick={(e) => handleClick(e, id)}
-              >
-                <div className={S.inner_box}>
-                  <div className={S.image_box}>
-                    <img src="/test-image.png" alt="가게 이미지"></img>
-                  </div>
-                  <div className={S.content_box}>
-                    <div className={S.brewery_name}>{breweryName}</div>
-                    <div className={S.brewery_desc}>{breweryDescription}</div>
-                    <div className={S.recommend_box}>
-                      <span className={S.recommend_title}>추천 맥주</span>
-                      <span className={S.recommend_beer}>
-                        {signatureBeer.beerName}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className={S.address}>
-                  <span>{fullAddress}</span>
-                </div>
-              </li>
-            );
-          }
-        )}
+        {breweries.map((brewery: Brewery) => {
+          return (
+            <BreweryCard brewery={brewery} saveSearchInfo={saveSearchInfo} />
+          );
+        })}
       </ul>
     </section>
   );
