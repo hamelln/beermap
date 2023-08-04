e42e9001635e060b615da26dfa3d6034ccf1718e
hamelln <vusdus91@gmail.com>
2023-08-04 15:11:33 +0900

    fix: json 대신 data 반환하도록 변경

--- client/src/services/BreweriesApi.ts
+++ client/src/services/BreweriesApi.ts
@@ -6,16 +6,17 @@ class BreweriesApi {
   private readonly baseUrl: string = "http://localhost:3008";
 
   async fetchBreweriesByInputText(query: string): Promise<Brewery[]> {
-    const breweries: Brewery[] = await axios.post(
-      `${this.baseUrl}/search?q=${query}`
-    );
+    const breweries: Brewery[] = await axios
+      .post(`${this.baseUrl}/search?q=${query}`)
+      .then((res) => res.data);
     return breweries;
   }
 
   async fetchBreweryById(breweryId: string): Promise<Brewery | null> {
-    const brewery: Brewery = await axios.get(
-      `${this.baseUrl}/search/${breweryId}`
-    );
+    const brewery: Brewery = await axios
+      .get(`${this.baseUrl}/search/${breweryId}`)
+      .then((res) => res.data);
+
     return brewery ?? null;
   }
 }
